bd273ec62e3afbfc6926a4b077153b93dd1bb7fa
Денис Сахаров <stran.nik@mail.ru>
2019-01-30 22:39:41 +0600

    Инициализация проекта. Настроил подключение к БД. Создал пустую миграцию для сохрания распарсенных данных

---
+++ migrations/m190130_155846_table_kgd_info.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m190130_155846_table_kgd_info
+ */
+class m190130_155846_table_kgd_info extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m190130_155846_table_kgd_info cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m190130_155846_table_kgd_info cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
